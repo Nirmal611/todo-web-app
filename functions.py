8bf0e357ebe6e64834ee1833e390ded475347d6e
Nirmal611 <125339075+Nirmal611@users.noreply.github.com>
2024-09-07 20:05:29 +0530

    Update functions.py
    removed the full file path

--- functions.py
+++ functions.py
@@ -1,11 +1,11 @@
 def get_todos():
-    with open('C:/Users/pnirm/PycharmProjects/ToDoListGUIapp/.venv/todos.txt','r') as file:
+    with open('todos.txt','r') as file:
         todo=file.readlines()
         return todo
 
 
 def write_todos(todos):
-    with open('C:/Users/pnirm/PycharmProjects/ToDoListGUIapp/.venv/todos.txt','w') as file:
+    with open('todos.txt','w') as file:
         file.writelines(todos)
 
 
